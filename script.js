9405c482f2cac504ff120bf9c18fee99f44b9497
atharavsawnt95 <atharavsawnt95@gmail.com>
2024-03-19 13:44:31 +0530

    add code

---
+++ script.js
@@ -0,0 +1,35 @@
+
+var data="";
+
+function HandleResult(value){
+    document.getElementById('Screen').innerHTML=value;
+}
+
+
+function handleNumbers(number){
+    data+=number;
+    console.log(data);
+    HandleResult(data);
+}
+
+
+function handleOperations(operator){
+    data+=operator;
+    console.log(data);
+    HandleResult(data);
+}
+
+function clearDisplay(){
+    var clear=0;
+    data=clear;
+    HandleResult("");
+    console.log(clear);
+    HandleResult(clear);
+}
+
+
+function GetResult(){
+    var result=eval(data);
+    console.log(result);
+    HandleResult(result);
+}
